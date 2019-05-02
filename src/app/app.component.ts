578f686c8fe2e7729325baa13e40a465963d2a59
Sagar Ratnaparkhi <sagar.ibmr@gmail.com>
2019-05-02 09:07:08 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: 'app-root',
+  template: `
+    <mat-toolbar color="primary"> <span>LocalCast Weather</span> </mat-toolbar>
+    <div fxLayoutAlign="center">
+      <div class="mat-caption vertical-margin">Your city, your forecast, right now!</div>
+    </div>
+    <div fxLayoutAlign="center">
+      <app-city-search></app-city-search>
+      <!-- <app-city-search-tpldriven></app-city-search-tpldriven> -->
+    </div>
+    <div fxLayout="row">
+      <div fxFlex></div>
+      <mat-card fxFlex="300px">
+        <mat-card-header>
+          <mat-card-title><div class="mat-headline">Current Weather</div></mat-card-title>
+        </mat-card-header>
+        <mat-card-content> <app-current-weather></app-current-weather> </mat-card-content>
+      </mat-card>
+      <div fxFlex></div>
+    </div>
+  `,
 })
-export class AppComponent  {
-  name = 'Angular';
-}
+export class AppComponent {}
